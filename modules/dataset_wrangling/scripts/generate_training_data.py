1507bb5ef811465b28411dee3bf6b5ebcef833c8
Pranav Suryadevara <suryadevarapranav28@gmail.com>
2024-10-24 12:30:49 -0500

    Enviroment Setup

---
+++ modules/dataset_wrangling/scripts/generate_training_data.py
@@ -0,0 +1,18 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
+from src.paths import ROOT_PATH, DATA_PATH
+
+
+def generate_data():
+    pass
+
+
+if __name__ == "__main__":
+    # [TODO] Importing the examples file as a json.
+    # [TODO] Authenticate OPENAI
+    # [TODO] Construct Prompts
+    # [TODO] API Calls to OPENAI
+    # [TODO] Save Responses to training_data.json
+    pass
---
+++ modules/dataset_wrangling/src/paths.py
@@ -0,0 +1,7 @@
+import os
+import sys
+from pathlib import Path
+
+ROOT_PATH = Path(os.path.dirname(__file__)).parent.resolve()
+
+DATA_PATH = ROOT_PATH / "data"
