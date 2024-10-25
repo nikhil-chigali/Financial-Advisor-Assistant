2d2c08bf7f45ad0ca8613ed82dfaed8b883ef494
Pranav Suryadevara <suryadevarapranav28@gmail.com>
2024-10-25 01:21:38 -0500

    Implemented Logic for Generating Data using OpenAI

--- modules/dataset_wrangling/scripts/generate_training_data.py
+++ modules/dataset_wrangling/scripts/generate_training_data.py
@@ -1,18 +1,82 @@
+from typing import Dict, List
+from argparse import ArgumentParser
+
 import os
 import sys
+import json
+import dspy
+
+
+from tqdm import tqdm
+from loguru import logger
+from dotenv import load_dotenv
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from src.paths import ROOT_PATH, DATA_PATH
+from src.data_gen import GenerateSuggestions
 
 
-def generate_data():
-    pass
+def configure_dspy(model_name: str) -> None:
+
+    load_dotenv()
+    if "OPENAI_API_KEY" in os.environ:
+        logger.debug("API Key is Available..")
+
+    logger.info(f"Setting DSPY LM: {model_name}")
+    lm = dspy.LM(model_name)
+    dspy.configure(lm=lm)
+
+
+def generate_data(examples: List[Dict]) -> List[Dict]:
+    lm_module = GenerateSuggestions()
+
+    logger.info("Generating responses for the examples")
+    data = []
+    for i, example in tqdm(
+        enumerate(examples), desc="Generating response", total=len(examples)
+    ):
+        output = lm_module(**example)
+        reason, response = output.reasoning, output.response
+        example["answer"] = f"{reason} {response}"
+
+        data.append(example)
+
+        if i == 5:
+            break
+
+    return data
+
+
+def load_examples() -> Dict:
+
+    logger.info(f"Loading the Exapmles from Path - {DATA_PATH / 'exmaples.json'}")
+    with open(DATA_PATH / "examples.json", "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+    logger.info(f"Successfully loaded {len(data)} examples.")
+    return data
+
+
+def main(model_name: str) -> None:
+    configure_dspy(model_name)
+
+    examples = load_examples()
+
+    data = generate_data(examples)
+    logger.info(f"Saving {len(data)} examples to {DATA_PATH / 'training_data.json'}")
+    with open(DATA_PATH / "training_data.json", "w", encoding="utf-8") as file:
+        json.dump(data, file)
 
 
 if __name__ == "__main__":
-    # [TODO] Importing the examples file as a json.
-    # [TODO] Authenticate OPENAI
-    # [TODO] Construct Prompts
-    # [TODO] API Calls to OPENAI
-    # [TODO] Save Responses to training_data.json
-    pass
+
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--model",
+        type=str,
+        default="openai/gpt-4o-mini",
+        choices=["openai/gpt-4o", "openai/gpt-4o-mini", "openai/gpt-3.5-turbo"],
+    )
+    args = parser.parse_args()
+
+    main(args.model)
---
+++ modules/dataset_wrangling/src/data_gen.py
@@ -0,0 +1,44 @@
+"""
+    This script contains the dspy classes for generating LLM suggestions given user's information and context.
+"""
+
+import dspy
+
+
+class ResponseSignature(dspy.Signature):
+    """Generate a tailored answer to the user's question using the context and user's infomration"""
+
+    user_query = dspy.InputField(
+        desc="Information about the user followed by a question"
+    )
+    context = dspy.InputField(
+        desc="An extract from a news article that may be used to answer the user's query"
+    )
+    response = dspy.OutputField(
+        desc="Justified answer to the user's question within 100 tokens"
+    )
+
+
+class GenerateSuggestions(dspy.Module):
+    """
+    DSPY module for generating LLM responses for user's query provided the context by using Chain of Thought reasoning.
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.cot = dspy.ChainOfThought(ResponseSignature)
+
+    def forward(self, about_me: str, context: str) -> str:
+        """
+        Forward function takes user's info and the context.
+        Generates LLM response using Chain of Thought reasoning.
+
+        Args:
+            about_me (str): User's Information and Query.
+            context (str): Relevant factoid for answering the Query.
+
+        Returns:
+            output (Dict): Returns reasoning and response for the given query.
+        """
+        output = self.cot(user_query=about_me, context=context)
+        return output
